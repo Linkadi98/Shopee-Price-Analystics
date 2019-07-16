d73ce51888b5939c22927bd4f271f4a8ef0ec9c4
Pham Ngoc Minh <minhngock18@gmail.com>
2019-07-16 10:27:49 +0700

    Uploaded file to new repo branch UI

---
+++ Shopee Prices Analystic/Controllers/RegisterViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RegisterViewController.swift
+//  Shopee Prices Analystic
+//
+//  Created by Minh Pham on 7/14/19.
+//  Copyright © 2019 SAPO. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    //MARK: - Properties
+    @IBOutlet var groupTextField: [UITextField]!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        for textField in groupTextField {
+            textField.createUnderlineTextField()
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Shopee Prices Analystic/Controllers/SPAScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SPAScreenViewController.swift
+//  Shopee Prices Analystic
+//
+//  Created by Minh Pham on 7/12/19.
+//  Copyright © 2019 SAPO. All rights reserved.
+//
+
+import UIKit
+import TransitionButton
+
+class SPAScreenViewController: CustomTransitionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
