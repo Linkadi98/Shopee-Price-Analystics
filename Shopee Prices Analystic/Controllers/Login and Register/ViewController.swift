035e7194db63362385a678f024fdd3ae899345ab
Pham Ngoc Minh <minhngock18@gmail.com>
2019-07-22 23:34:55 +0700

    Changed Account tab to table style, added function to check valid email

--- Shopee Prices Analystic/Controllers/Login and Register/ViewController.swift
+++ Shopee Prices Analystic/Controllers/Login and Register/ViewController.swift
@@ -13,7 +13,7 @@ import FBSDKLoginKit
 import GoogleSignIn
 import NotificationBannerSwift
 
-class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, GIDSignInUIDelegate, GIDSignInDelegate {
 
     // MARK: - Properties
     
@@ -22,6 +22,13 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     @IBOutlet weak var loginButton: TransitionButton!
     @IBOutlet weak var facebookButton: UIButton!
     @IBOutlet weak var googleButton: UIButton!
+    
+    @IBOutlet weak var invalidUserName: UILabel! {
+        didSet {
+            invalidUserName.text = nil
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,15 +74,11 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
             
             sleep(3) // 3: Do your networking task or background work here.
             
+            // tạo một hàm để kết nối đến api check username password, trả về Bool
+            
             DispatchQueue.main.async(execute: { () -> Void in
-                // 4: Stop the animation, here you have three options for the `animationStyle` property:
-                // .expand: useful when the task has been compeletd successfully and you want to expand the button and transit to another view controller in the completion callback
-                // .shake: when you want to reflect to the user that the task did not complete successfly
-                // .normal
-                sender.stopAnimation(animationStyle: .expand, completion: {
-                    let secondVC = self.storyboard?.instantiateViewController(withIdentifier: "TabsViewController") as! TabsViewController
-                    self.present(secondVC, animated: false, completion: nil)
-                })
+                
+//
             })
         })
     }
@@ -92,4 +95,43 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     @IBAction func unWind(unWindSegue: UIStoryboardSegue) {
         
     }
+    
+    @IBAction func userNameEdittingDidChange(_ sender: Any) {
+        if !userNameText.isValidUserName() {
+            invalidUserName.text = "Vui lòng nhập tên đăng nhập hoặc email hợp lệ"
+        }
+        else {
+            invalidUserName.text = nil
+        }
+    }
+    
+    // MARK: - Private Modifications
+    
+    private func showInvalidMessage(with status: Bool = false, _ sender: TransitionButton) {
+        if !status {
+            sender.stopAnimation(animationStyle: .normal, revertAfterDelay: 1, completion: {
+                self.loginButton.layer.cornerRadius = self.loginButton.frame.height / 2
+                let alert = UIAlertController(title: "Đăng nhập không thành công", message: "Sai tên đăng nhập hoặc mật khẩu", preferredStyle: .alert)
+                let retyping = UIAlertAction(title: "Nhập lại", style: .default, handler: {(action) in
+                    self.passwordText.text = nil
+                })
+                alert.addAction(retyping)
+                alert.popoverPresentationController?.sourceView = self.view
+                self.present(alert, animated: true, completion: nil)
+            })
+        }
+            
+    }
+    
+    private func showMainAppicationAfterSuccessfulLogin(with status: Bool = true, _ sender: TransitionButton) {
+        if status {
+            sender.stopAnimation(animationStyle: .expand, completion: {
+            let secondVC = self.storyboard?.instantiateViewController(withIdentifier: "TabsViewController") as! TabsViewController
+            self.present(secondVC, animated: false, completion: nil)
+            })
+        }
+        
+    }
+    
+    
 }
---
+++ Shopee Prices Analystic/Models/Account.swift
@@ -0,0 +1,16 @@
+//
+//  Account.swift
+//  Shopee Prices Analystic
+//
+//  Created by Minh Pham on 7/22/19.
+//  Copyright © 2019 SAPO. All rights reserved.
+//
+
+import UIKit
+
+struct Account {
+    var accountImage: UIImage?
+    var accountName: String?
+//    var acountEmail: String?
+//    var accountPhone: String?
+}
